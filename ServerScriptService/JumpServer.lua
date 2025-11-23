fc151b6995f6da1892b69ccf72356e5e2caedbbb
tay <tnivens72gmail.com>
2025-11-23 02:13:31 -0500

    delete useless FUCKING code cus bad

--- ServerScriptService/JumpServer.lua
+++ ServerScriptService/JumpServer.lua
@@ -1,21 +1,7 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
-
 local doubleJumpEvent = ReplicatedStorage.RemoteEvents:WaitForChild("DoubleJumpEvent")
-
 local jumpEffect = ReplicatedStorage:FindFirstChild("JumpEffect")
-if not jumpEffect then
-	jumpEffect = Instance.new("Part")
-	jumpEffect.Name = "JumpEffect"
-	jumpEffect.Size = Vector3.new(0.5, 4, 4)
-	jumpEffect.Transparency = 0.5
-	jumpEffect.Anchored = true
-	jumpEffect.CanCollide = false
-	jumpEffect.Material = Enum.Material.Neon
-	jumpEffect.Shape = Enum.PartType.Cylinder
-	jumpEffect.Color = Color3.fromRGB(255, 255, 255)
-	jumpEffect.Parent = ReplicatedStorage
-end
 
 doubleJumpEvent.OnServerEvent:Connect(function(player)
 	for _, otherPlayer in ipairs(Players:GetPlayers()) do
@@ -24,3 +10,4 @@ doubleJumpEvent.OnServerEvent:Connect(function(player)
 		end
 	end
 end)
+-- this script literally just makes it so eveyrone can see the jump effect
